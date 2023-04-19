d5d4db356e23da87417ce214f95c719a976526f6
esraemre <eesraemre@outlook.com>
2023-04-19 12:20:00 +0100

    deneme

--- src/A03.java
+++ src/A03.java
@@ -1,5 +1,9 @@
 public class A03 {
     public static void main(String[] args) {
 
+        String text="text";
+
+        System.out.println(text);
+
     }
 }
---
+++ src/a05.java
@@ -0,0 +1,5 @@
+public class a05 {
+    public static void main(String[] args) {
+
+    }
+}
